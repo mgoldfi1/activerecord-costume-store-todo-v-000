1fcfcb723b90bffc94819dc040dc7a828ee350b7
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-23 14:29:07 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
 # Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration
+
+  def change 
+    create_table :haunted_houses do |x|
+      x.string :name
+      x.string :location
+      x.string :theme
+      x.integer :price
+      x.boolean :family_friendly
+      x.string :opening_date
+      x.string :closing_date
+      x.string :long_description
+    end
+  end
