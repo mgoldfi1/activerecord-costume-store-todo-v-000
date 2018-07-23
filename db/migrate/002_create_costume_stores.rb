532bdba614f9b21456cbe107955a2ee1ed1d1581
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-07-23 14:41:20 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -8,8 +8,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
       x.integer :costume_inventory
       x.integer :num_of_employees
       x.boolean :still_in_business
-      x.integer :opening_time
-      x.integer :closing_time
+      x.datetime :opening_time
+      x.datetime :closing_time
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -18,8 +18,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "costume_inventory"
     t.integer "num_of_employees"
     t.boolean "still_in_business"
-    t.string "opening_time"
-    t.string "closing_time"
+    t.integer "opening_time"
+    t.integer "closing_time"
   end
 
   create_table "costumes", force: :cascade do |t|
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.integer "price"
     t.string "size"
     t.string "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
